a0ed96b245bb5df21ad716f0a6a5c502eabe36ea
johnrefani <jhnrefani@gmail.com>
2025-01-14 13:18:42 +0800

    Refactor UI/UX and Components

--- Assets/Scripts/Loading Page/Loading.cs
+++ Assets/Scripts/Loading Page/Loading.cs
@@ -2,49 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class Loading : MonoBehaviour
 {
     public string OnboardingPage = "PretestPage";
     public string StartMenuPage = "StartingPage";
-    public float delay = 4.5f;
+    public VideoPlayer videoplayer;
 
     void Start()
+    {
+        videoplayer.loopPointReached += OnVideoEnd;
+        videoplayer.Play();
+    }
+
+    void OnVideoEnd (VideoPlayer vp)
     {
         int onboardingStatus = PlayerPrefs.GetInt("OnboardingStatus", 0);
 
         if (onboardingStatus == 0)
         {
-            // Play onboarding
-            Debug.Log("Playing Pretest");
 
-            // Add a 5-second delay before logging
-            Invoke("Pretest", delay);
+            SceneManager.LoadScene(OnboardingPage);
         }
         else if (onboardingStatus == 1)
         {
-            // Play intro
-            Debug.Log("Playing Intro");
 
-            // Add a 5-second delay before logging
-            Invoke("MainMenu", delay);
+            SceneManager.LoadScene(StartMenuPage);
         }
     }
 
-    void Pretest()
-    {
-        Debug.Log("Logged Intro after 5 seconds");
-        // Add any additional code you want to execute after the delay
-
-        SceneManager.LoadScene(OnboardingPage);
-    }
-
-    void MainMenu()
-    {
-        Debug.Log("Logged Intro after 5 seconds");
-        // Add any additional code you want to execute after the delay
-
-        SceneManager.LoadScene(StartMenuPage);
-    }
 }
 
